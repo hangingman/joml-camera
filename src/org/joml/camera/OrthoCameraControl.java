47543761f5b71abf959221ae92777bdc738bbec0
httpdigest <kburjack@googlemail.com>
2017-04-20 14:04:20 +0200

    Add support for rotation in OrthoCameraControl

--- src/org/joml/camera/OrthoCameraControl.java
+++ src/org/joml/camera/OrthoCameraControl.java
@@ -1,6 +1,8 @@
 package org.joml.camera;
 
+import org.joml.Math;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
@@ -22,16 +24,25 @@ import org.joml.Vector4f;
  */
 public class OrthoCameraControl {
 
+    public static int MOUSE_LEFT = 0;
+    public static int MOUSE_RIGHT = 1;
+    public static int MOUSE_CENTER = 2;
+
     private Matrix4f view = new Matrix4f();
     private Matrix4f viewproj = new Matrix4f();
     private Matrix4f invviewproj = new Matrix4f();
     private int[] vp = new int[4];
     private float mouseX, mouseY;
-    private boolean mouseDown;
+    private float mouseDownX, mouseDownY;
+    private boolean[] mouseDown = new boolean[3];
     private Vector3f v = new Vector3f();
+    private Quaternionf q = new Quaternionf();
+
+    private float minRotateWinDistance = 100.0f;
 
     /**
-     * @param extents the initial extents in world coordinates
+     * @param extents
+     *            the initial extents in world coordinates
      */
     public OrthoCameraControl(float extents) {
         view.setOrtho2D(-extents, extents, -extents, extents);
@@ -39,8 +50,20 @@ public class OrthoCameraControl {
     }
 
     /**
-     * @param width the width of the control/window in window coordinates/pixels
-     * @param height the height of the control/window in window coordinates/pixels
+     * @param minRotateWinDistance
+     *            the minimum distance in window coordinates/pixels between
+     *            the mouse down position and the current mouse position so
+     *            that rotation is allowed
+     */
+    public void setMinRotateWinDistance(float minRotateWinDistance) {
+        this.minRotateWinDistance = minRotateWinDistance;
+    }
+
+    /**
+     * @param width
+     *            the width of the control/window in window coordinates/pixels
+     * @param height
+     *            the height of the control/window in window coordinates/pixels
      */
     public void setSize(int width, int height) {
         vp[0] = 0;
@@ -60,63 +83,94 @@ public class OrthoCameraControl {
     }
 
     /**
-     * @param x the x coordiante of the point to center on in world coordinates
-     * @param y the y coordiante of the point to center on in world coordinates
+     * @param x
+     *            the x coordiante of the point to center on in world coordinates
+     * @param y
+     *            the y coordiante of the point to center on in world coordinates
      */
     public void center(float x, float y) {
         view.setTranslation(0, 0, 0).translate(-x, -y, 0);
         update();
     }
 
-    public void onMouseDown() {
-        mouseDown = true;
+    public void onMouseDown(int button) {
+        mouseDownX = mouseX;
+        mouseDownY = mouseY;
+        mouseDown[button] = true;
     }
 
-    public void onMouseUp() {
-        mouseDown = false;
+    public void onMouseUp(int button) {
+        mouseDown[button] = false;
     }
 
     /**
-     * @param winX the x coordinate in window coordinates/pixels
-     * @param winY the y coordinate in window coordinates/pixels
+     * @param winX
+     *            the x coordinate in window coordinates/pixels
+     * @param winY
+     *            the y coordinate in window coordinates/pixels
      */
     public void onMouseMove(int winX, int winY) {
-        if (mouseDown) {
+        if (mouseDown[MOUSE_LEFT]) {
+            /* Move */
             invviewproj.unprojectInv(v.set(winX, winY, 0.0f), vp, v);
             float x0 = v.x, y0 = v.y;
             invviewproj.unprojectInv(v.set(mouseX, mouseY, 0.0f), vp, v);
             float x1 = v.x, y1 = v.y;
             view.translate(x0 - x1, y0 - y1, 0.0f);
             update();
+        } else if (mouseDown[MOUSE_RIGHT]) {
+            /* Check if rotation is already possible */
+            float dx = winX - mouseDownX;
+            float dy = winY - mouseDownY;
+            float d = (float) Math.sqrt(dx * dx + dy * dy);
+            if (d > minRotateWinDistance) {
+                /* Rotate */
+                float dx0 = winX - mouseDownX, dy0 = winY - mouseDownY;
+                float dx1 = mouseX - mouseDownX, dy1 = mouseY - mouseDownY;
+                float ang = (float) Math.atan2(dx0 * dy1 - dy0 * dx1, dx0 * dx1 + dy0 * dy1);
+                Vector3f ndc = ndc(mouseDownX, mouseDownY);
+                view.rotateAroundLocal(q.rotationZ(-ang), ndc.x, ndc.y, 0.0f);
+                update();
+            }
         }
         mouseX = winX;
         mouseY = winY;
     }
 
+    private Vector3f ndc(float winX, float winY) {
+        float x = (winX / vp[2] * 2.0f - 1.0f) * (vp[2] / vp[3]);
+        float y = winY / vp[3] * 2.0f - 1.0f;
+        return v.set(x, y, 0.0f);
+    }
+
     /**
-     * @param scale the scale factor. &lt; 1.0 to zoom out; &gt; 1.0 to zoom in
+     * @param scale
+     *            the scale factor. &lt; 1.0 to zoom out; &gt; 1.0 to zoom in
      */
     public void zoom(float scale) {
-        float x = (mouseX / vp[2] * 2.0f - 1.0f) * (vp[2] / vp[3]);
-        float y = mouseY / vp[3] * 2.0f - 1.0f;
-        view.scaleAroundLocal(scale, x, y, 0);
+        Vector3f ndc = ndc(mouseX, mouseY);
+        view.scaleAroundLocal(scale, ndc.x, ndc.y, 0);
         update();
     }
 
     /**
-     * @param dest contains the view rectangle as {x: minX, y: minY, z: width, w: height}
+     * @param dest
+     *            contains the view rectangle as {x: minX, y: minY, z: width, w: height}
      * @return dest
      */
     public Vector4f viewRect(Vector4f dest) {
-        invviewproj.transform(-1, -1, 0, 1, dest);
-        float nX = dest.x, nY = dest.y;
-        invviewproj.transform(+1, +1, 0, 1, dest);
-        float pX = dest.x, pY = dest.y;
-        dest.x = nX; // <- the minimum x coordinate
-        dest.y = nY; // <- the minimum y coordinate
-        dest.z = pX - nX; // <- the visible width
-        dest.w = pY - nY; // <- the visible height
-        return dest;
+        float minX = Float.POSITIVE_INFINITY, minY = Float.POSITIVE_INFINITY;
+        float maxX = Float.NEGATIVE_INFINITY, maxY = Float.NEGATIVE_INFINITY;
+        for (int i = 0; i < 4; i++) {
+            float x = ((i & 1) << 1) - 1.0f;
+            float y = (((i >>> 1) & 1) << 1) - 1.0f;
+            invviewproj.transformPosition(v.set(x, y, 0.0f));
+            minX = minX < v.x ? minX : v.x;
+            minY = minY < v.y ? minY : v.y;
+            maxX = maxX > v.x ? maxX : v.x;
+            maxY = maxY > v.y ? maxY : v.y;
+        }
+        return dest.set(minX, minY, maxX, maxY);
     }
 
 }
